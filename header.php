c003b31069b99398b8780111e32198f00a412895
Dimitri Bertolami <dimibertolami@gmail.com>
2022-07-01 12:07:07 +0200

    found most code online but I understand it better now!

---
+++ header.php
@@ -0,0 +1,8 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Flat-File Guestbook</title>
+    <link href="style.css" rel="stylesheet">
+</head>
